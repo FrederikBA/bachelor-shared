e158ab12d60827af91a47e13b8e40475ff2cbb25
FrederikBA <frederikbandersen98@gmail.com>
2024-04-05 16:37:05 +0200

    Shared version - 8.0.1

--- src/Shared/Integration/Models/SharedTestPerson.cs
+++ src/Shared/Integration/Models/SharedTestPerson.cs
@@ -6,4 +6,5 @@ public class SharedTestPerson
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public int Age { get; set; }
+    public string? Email { get; set; }
 }
