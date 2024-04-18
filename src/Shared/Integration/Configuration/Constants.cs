1fdf57036576e7d6b66f85925848c8ee0bf0888a
FrederikBA <frederikbandersen98@gmail.com>
2024-04-18 09:30:44 +0200

    Integration - add configuration constants

---
+++ src/Shared/Integration/Configuration/Constants.cs
@@ -0,0 +1,6 @@
+namespace Shared.Integration.Configuration;
+
+public static class Constants
+{
+    public const string JwtKey = "3hfJ14K+20sXCy+GYCZjv12bM8Bw2m8F5VtOM3tN+F6KuVI51ohrtJEN+vQ0sFm2";
+}
