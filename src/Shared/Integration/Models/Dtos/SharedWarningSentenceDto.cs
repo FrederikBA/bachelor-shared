353efa2b69d9c5db8af21e99fbd4a17c7a09bf18
FrederikBA <frederikbandersen98@gmail.com>
2024-04-20 15:15:09 +0200

    json properties on shared dtos

--- src/Shared/Integration/Models/Dtos/SharedWarningSentenceDto.cs
+++ src/Shared/Integration/Models/Dtos/SharedWarningSentenceDto.cs
@@ -1,7 +1,12 @@
+using System.Text.Json.Serialization;
+
 namespace Shared.Integration.Models.Dtos;
 
 public class SharedWarningSentenceDto
 {
+    [JsonPropertyName("warningSentenceId")]
     public int WarningSentenceId { get; set; }
+
+    [JsonPropertyName("warningSentenceCode")]
     public string? WarningSentenceCode { get; set; }
 }
